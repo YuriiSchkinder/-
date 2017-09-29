68d0a8258013dc48877c95c0f9af1d3191f9f9fd
Yrii Schkinder <yurkaaa96@gmail.com>
2017-09-29 21:18:14 +0300

    сортіровка

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$arr=[
+    'page51.php',
+    'page4000.php',
+    'page9.php'
+];
+$str='';
+$arr2=array();
+foreach($arr as $key=>$value){
+   foreach(str_split($value) as $a){
+       if ($a>='0' && $a<='9'){
+           $str.=$a;
+       }
+   }
+   settype($str,"integer");
+    $arr2[$str]=$value;
+    $str='';
+}
+ksort($arr2);
+foreach($arr2 as $value){
+    echo $value."<br>";
+}
+?>
